b4757198a33d5dd4aff325900890e52326bf44d0
DangerGreen <r.malikgreen@gmail.com>
2018-12-19 10:23:00 -0800

    added payment form html, css, and js

---
+++ config/local-production.js
@@ -0,0 +1,9 @@
+const config = {
+    square: {
+        applicationID: "sq0idp-L3Zq8zqhsQ4Lfebheybxyg",
+        personalAccessToken: "sq0atp-9xAjm4X9WdaIN-W9JR5dEA"
+
+    }
+  };
+  
+  module.exports = config;
---
+++ config/local-sandbox.js
@@ -0,0 +1,9 @@
+const config = {
+    square: {
+        applicationID: "sandbox-sq0idp-L3Zq8zqhsQ4Lfebheybxyg",
+        personalAccessToken: "sandbox-sq0atb-FlUyV4j3KaAIl7fwqfrnSg"
+
+    }
+  };
+  
+  module.exports = config;
